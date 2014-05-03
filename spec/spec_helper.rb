5a8c6baa2b5d6a49cace5c57bac034f38f1878c1
Michael Pope <amorphid@captaincoder.io>
2014-05-03 13:57:44 -0700

    Add poltergeist gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "rspec/autorun"
 require "capybara/rspec"
+require 'capybara/poltergeist'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -12,6 +13,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+# Driver for headless browser testing w/ support for JavaScript
+Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
