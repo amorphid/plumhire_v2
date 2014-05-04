40a5fa9d7742d9820c5b783186251000b08b1c3b
Michael Pope <amorphid@captaincoder.io>
2014-05-03 18:12:16 -0700

    Add root route pointing to JobsController#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "jobs#index"
+
   resources(
     :jobs,
     only: :index
--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe JobsController do
   it "routes to #index" do
+    get("/").should route_to("jobs#index")
     get("/jobs").should route_to("jobs#index")
   end
 end
