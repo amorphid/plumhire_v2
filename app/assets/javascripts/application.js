85b1358e2805a92b8a3701cfa861d0097bbf14c6
Michael Pope <amorphid@captaincoder.io>
2014-05-03 18:52:40 -0700

    Add new job link to jobs index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require bootswatch
 //= require_tree .
 
--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -5,4 +5,8 @@ describe JobsController do
     get("/").should route_to("jobs#index")
     get("/jobs").should route_to("jobs#index")
   end
+
+  it "routes to #new" do
+    get("/jobs/new").should route_to("jobs#new")
+  end
 end
