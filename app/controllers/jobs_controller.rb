682d87be5d67bf3563336b112f763c7828e8c0f6
Michael Pope <amorphid@captaincoder.io>
2014-05-04 12:46:58 -0700

    Add update method to JobsController

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,14 @@ class JobsController < ApplicationController
   def new
     @job = Job.new(id: SecureRandom.uuid)
   end
+
+  def update
+    @job = Job.find_or_initialize_by(id: params[:id])
+    @job.update(job_params)
+    render :new
+  end
+
+  def job_params
+    params[:job].permit(:body, :title)
+  end
 end
