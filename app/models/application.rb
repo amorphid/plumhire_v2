347a93ebe7bdd6492d5a732291fef009c0143957
Michael Pope <amorphid@captaincoder.io>
2014-05-05 15:51:07 -0700

    Refactor Application model tests and make tests pass

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,18 @@
 class Application < ActiveRecord::Base
+  validates(
+    :email,
+    presence: true,
+    uniqueness: true
+  )
+
+  validates(
+    :name,
+    presence: true
+  )
+
+  validates(
+    :resume_url,
+    presence: true,
+    uniqueness: true
+  )
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,46 +4,40 @@ describe Application do
   subject do
     Fabricate.build(
       :application,
-      email: "unique@example.com",
-      resume_url:   "https://unique.example.com"
+      email:      "unique@example.com",
+      resume_url: "https://unique.example.com"
     )
   end
 
-  context "#email" do
-    before { expect(subject).to have(0).errors_on(:email) }
+  before { expect(subject).to be_valid }
 
+  context "#email" do
     it "validates presence" do
-      expect(subject).to have(0).errors_on(:email)
       subject.email = ""
       expect(subject).to have(1).errors_on(:email)
     end
 
     it "validates uniqueness" do
-      subject.save
-      subject.email = "not_unique@example.com"
+      Fabricate(:application, email: "unique@example.com")
       expect(subject).to have(1).errors_on(:email)
     end
   end
 
   context "#name" do
     it "validates presence" do
-      expect(subject).to have(0).errors_on(:name)
       subject.name = ""
       expect(subject).to have(1).errors_on(:name)
     end
   end
 
-  context "#url" do
-    before { expect(subject).to have(0).errors_on(:resume_url) }
-
+  context "#resume_url" do
     it "validates presence" do
       subject.resume_url = ""
       expect(subject).to have(1).errors_on(:resume_url)
     end
 
     it "validates uniqueness" do
-      subject.save
-      subject.resume_url = "not_unique.example.com"
+      Fabricate(:application, resume_url: "https://unique.example.com")
       expect(subject).to have(1).errors_on(:resume_url)
     end
   end
